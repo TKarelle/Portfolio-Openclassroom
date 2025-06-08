63a4ad2e457372beac2813d57c9d6dfb47a639f9
Karelle table <karelle.tble@gmail.com>
2025-06-08 11:49:57 +0200

    Initial commit

--- components/Button/index.js
+++ components/Button/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import { useTheme } from "next-themes";
 import data from "../../data/portfolio.json";
 
-const Button = ({ children, type, onClick, classes }) => {
+const Button = React.forwardRef(({ children, type, onClick, classes }, ref) => {
   const { theme } = useTheme();
   if (type === "primary") {
     return (
       <button
+        ref={ref}
         onClick={onClick}
         type="button"
         className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg ${
@@ -21,6 +22,7 @@ const Button = ({ children, type, onClick, classes }) => {
   }
   return (
     <button
+      ref={ref}
       onClick={onClick}
       type="button"
       className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg flex items-center transition-all ease-out duration-300 ${
@@ -34,6 +36,8 @@ const Button = ({ children, type, onClick, classes }) => {
       {children}
     </button>
   );
-};
+});
+
+Button.displayName = "Button";
 
 export default Button;
